cfe6a2bbb4098b75b7da0028ef4f13b4f332d8e9
root <root@4cc4dd807910>
2017-06-21 09:35:43 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "flyer",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "SalesVisionaPPfromShell",
+  "homePage" : "Main",
+  "name" : "SalesVisionaPPfromShell",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
